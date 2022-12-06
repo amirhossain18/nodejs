c0f99459470e85d6ed257d2baa2683e337eae9e2
amirhossainbond <67521933+amirhossainbond@users.noreply.github.com>
2022-12-06 22:25:49 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express= require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+const cors = require('cors')
+app.use(cors())
+// const category = require('../modern-tech-server/Catagory.json')
+// const catagoryDetails = require('../modern-tech-server/CourseDetails.json')
+
+app.get('/', (req, res) =>{
+    res.send('wellcome')
+})
+app.get('/courses', (req, res) =>{
+    res.send('hello from course route sdfasdf')
+})
+
+// app.get('/courses/:id', (req, res) =>{
+//     // res.send(categorys)
+//     const id = req.params.id;
+//     console.log(id)
+//     const selectedCourse = category.find(c => c.id === id);
+//     res.send(selectedCourse)
+//     console.log(selectedCourse)
+// })
+
+app.listen(port , ()=> {
+    
+    console.log('Your port is runnig', port)
+})
